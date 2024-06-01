4f4678455b870dd9266c8c104761a0b54ba582f5
Donovan <donovan.courtney@gmail.com>
2024-05-31 20:00:05 -0500

    added recharts for issues chart. styled dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,31 @@
 import prisma from "@/prisma/client";
 import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
+import IssueChart from "./IssueChart";
+import { Flex, Grid } from "@radix-ui/themes";
 
 const Home = async () => {
   const open = await prisma.issue.count({ where: { status: "OPEN" } });
-  const inProgress = await prisma.issue.count({ where: { status: "IN_PROGRESS" } });
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
   const closed = await prisma.issue.count({ where: { status: "CLOSED" } });
 
-  
-  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
+  const statuses = {
+    open: open,
+    inProgress: inProgress,
+    closed: closed,
+  };
+
+  return (
+    <Grid columns={{ initial: "1", md: "2" }} gap={"5"}>
+      <Flex direction={"column"} gap="5">
+        <IssueSummary {...statuses} />
+        <IssueChart {...statuses} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
 };
 
 export default Home;
